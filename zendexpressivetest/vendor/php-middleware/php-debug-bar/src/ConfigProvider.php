b6d560477480858a81918c13467b39cbd97c0f1c
Sieciech <wojciech.mocek@o2.pl>
2019-07-23 02:47:16 +0200

    added vendor to git

---
+++ zendexpressivetest/vendor/php-middleware/php-debug-bar/src/ConfigProvider.php
@@ -0,0 +1,21 @@
+<?php
+declare (strict_types=1);
+
+namespace PhpMiddleware\PhpDebugBar;
+
+final class ConfigProvider
+{
+    public static function getConfig(): array
+    {
+        return (new self())();
+    }
+
+    public function __invoke(): array
+    {
+        $config = include __DIR__ . '/../config/phpdebugbar.config.php';
+        $config['dependencies'] = include __DIR__ . '/../config/dependency.config.php';
+        $config['middleware_pipeline'] = include __DIR__ . '/../config/zend-expressive.middleware_pipeline.config.php';
+
+        return $config;
+    }
+}
