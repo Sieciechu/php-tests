0e411c45e556b8637361209facf4ec824735ed86
Wojciech <wojciech.mocek@o2.pl>
2020-03-09 20:23:56 +0100

    Update app.php

--- mailhogtest/app.php
+++ mailhogtest/app.php
@@ -1,7 +1,12 @@
 <?php
 $from = 'test_from@gmail.com';
 $to = 'your.emailaddress@gmail.com';
-$x = mail($to, 'subject'.time(), 'Hello World', 'From: '. $from);
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+$headers .= 'From: '. $from . "\r\n";
+
+$x = mail($to, 'subject'.time(), '<html><body>This should be html email<br/>Thank you</body></html>', $headers);
 var_dump($x);
 
 echo PHP_EOL;
